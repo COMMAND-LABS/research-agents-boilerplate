10637aeb5095d8ef72fa6a0da11826f4db1f056f
thaddavis <thaddadavis@gmail.com>
2025-01-18 01:03:20 -0500

    making boilerplate slicker

--- scripts/generate_service_account.sh
+++ scripts/generate_service_account.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
+PROJECT_NUMBER=<YOUR_PROJECT_NUMBER_HERE>
+PROJECT_ID=<YOUR_PROJECT_ID_HERE>
 SERVICE_ACCOUNT_NAME="research-agents-cicd"
+SERVICE_ACCOUNT_EMAIL="research-agents-cicd@$PROJECT_ID.iam.gserviceaccount.com"
+SERVICE_ACCOUNT_DESCRIPTION="Service account description"
+SERVICE_ACCOUNT_DISPLAY_NAME="Service account display name"
 SERVICE_ACCOUNT_KEY_FILE="research-agents-cicd-sa.json"
-ARTIFACTORY_REPO_NAME=research-agents-repo
-PROJECT_NUMBER=830723611668
+ARTIFACTORY_REPO_NAME=research-agents-artifactory-repo
 
 gcloud iam service-accounts create $SERVICE_ACCOUNT_NAME \
-  --description="Service account for GitHub Actions CI/CD for the Research Agents" \
-  --display-name="Research Agents CICD"
+  --description=$SERVICE_ACCOUNT_DESCRIPTION \
+  --display-name=$SERVICE_ACCOUNT_DISPLAY_NAME
+
 
 gcloud artifacts repositories add-iam-policy-binding $ARTIFACTORY_REPO_NAME \
   --location=us-east1 \
-  --member="serviceAccount:$SERVICE_ACCOUNT_NAME@kalygo-436411.iam.gserviceaccount.com" \
-  --role="roles/artifactregistry.writer"
+  --member="serviceAccount:$SERVICE_ACCOUNT_EMAIL" \
+  --role="roles/artifactregistry.admin"
 
-gcloud projects add-iam-policy-binding kalygo-436411 \
-  --member="serviceAccount:$SERVICE_ACCOUNT_NAME@kalygo-436411.iam.gserviceaccount.com" \
-  --role="roles/artifactregistry.reader"
-
-gcloud projects add-iam-policy-binding kalygo-436411 \
-  --member="serviceAccount:$SERVICE_ACCOUNT_NAME@kalygo-436411.iam.gserviceaccount.com" \
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member="serviceAccount:$SERVICE_ACCOUNT_EMAIL" \
   --role="roles/secretmanager.secretAccessor"
 
-gcloud projects add-iam-policy-binding kalygo-436411 \
-  --member="serviceAccount:$SERVICE_ACCOUNT_NAME@kalygo-436411.iam.gserviceaccount.com" \
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member="serviceAccount:$SERVICE_ACCOUNT_EMAIL" \
   --role="roles/run.admin"
 
 gcloud iam service-accounts add-iam-policy-binding $PROJECT_NUMBER-compute@developer.gserviceaccount.com \
-  --member="serviceAccount:$SERVICE_ACCOUNT_NAME@kalygo-436411.iam.gserviceaccount.com" \
+  --member="serviceAccount:$SERVICE_ACCOUNT_EMAIL" \
   --role="roles/iam.serviceAccountUser"
 
 gcloud iam service-accounts keys create $SERVICE_ACCOUNT_KEY_FILE \
-  --iam-account="$SERVICE_ACCOUNT_NAME@kalygo-436411.iam.gserviceaccount.com"
+  --iam-account="$SERVICE_ACCOUNT_EMAIL"
